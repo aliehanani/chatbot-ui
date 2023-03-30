cb0ecbe957090f182d5addf3ad8285bd8fdf743c
aliehanani <aliehanani@users.noreply.github.com>
2023-03-30 14:32:16 +0700

    fix missing rebrand

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -157,7 +157,7 @@ export const Chat: FC<Props> = memo(
               Welcome to ChatGPT with Triffick
             </div>
             <div className="text-center text-lg text-black dark:text-white">
-              <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
+              <div className="mb-8">{`ChatGPT with Triffick is a clone of OpenAI's ChatGPT UI.`}</div>
               <div className="mb-2 font-bold">
                 Important: ChatGPT with Triffick is 100% unaffiliated with OpenAI.
               </div>
